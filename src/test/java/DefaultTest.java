ee4f8fe4320a71a66dba650517becb29f586b49a
Sam <samuelzheng11@gmail.com>
2019-05-14 17:37:36 +1200

    set up default test

---
+++ src/test/java/DefaultTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class DefaultTest {
+    @Test
+
+    public void defaultMethod1Test() {
+        Default defaultClass = new Default();
+        defaultClass.method1();
+    }
+}
