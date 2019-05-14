4345a1704c7a2f99e4d55d7edc985d8d02ba8bdc
Sam <samuelzheng11@gmail.com>
2019-05-14 18:34:41 +1200

    change to java 8

--- src/test/java/DefaultTest.java
+++ src/test/java/DefaultTest.java
@@ -2,7 +2,6 @@ import org.junit.Test;
 
 public class DefaultTest {
 
-    // Default test to test build on travis ci
     // TODO: remove this test when code starts to be integrated
     @Test
     public void defaultMethod1Test() {
