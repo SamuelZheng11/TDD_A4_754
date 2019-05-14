7930a4cb0a5b9f3ccdf808ca7896cf27f46562b6
Sam <samuelzheng11@gmail.com>
2019-05-14 17:55:00 +1200

    trigger build start

--- src/test/java/DefaultTest.java
+++ src/test/java/DefaultTest.java
@@ -2,6 +2,8 @@ import org.junit.Test;
 
 public class DefaultTest {
 
+    // Default test to test build on travis ci
+    // TODO: remove this test when code starts to be integrated
     @Test
     public void defaultMethod1Test() {
         Default defaultClass = new Default();
