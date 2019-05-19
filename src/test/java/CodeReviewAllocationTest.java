204ec1ff5572893133f5b2f273aa50d622e83056
JennyLee <0608jennylee@hotmail.co.nz>
2019-05-19 15:59:11 +1200

    add code review unit test for requirement 8 - does not compile(no code review class)

---
+++ src/test/java/CodeReviewAllocationTest.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class CodeReviewAllocationTest {
+    public String sourceBranchName = "GithubPullRequestFetchTest_Branch";
+    public String targetBranchName = "GithubPullRequestFetchTest_TargetBranch";
+
+    public User developer = new User("", UserType.Developer);
+    public User nonDeveloper = new User("", UserType.NonDeveloper);
+
+    public GitCommit commit = new GitCommit("Commit Message", "GithubPullRequestFetchTest_Commit");
+    public GitCommit[] committed_code = {commit};
+    public GitBranch sourceBranch = new GitBranch(sourceBranchName, committed_code);
+    public GitBranch targetBranch = new GitBranch(targetBranchName, committed_code);
+
+    private User _developer;
+    private GithubApi _github;
+
+    @Before
+    public void initialize(){
+        _github = new MockGithubModule();
+    }
+
+    /**
+     * 8. The developer can add/delete one or more non-developer reviewers in this
+     * tool. A database is used to store the reviewers’ information.
+     */
+    //Requirement(8)
+    @Test
+    public void TestDeveloperCanAddCodeReviewer() {
+        //Given
+        PullRequest pullRequest = _github.createPullRequest("Test add code reviewers", sourceBranch, targetBranch);
+        //When
+        CodeReview codeReview = new CodeReview(pullRequest, developer, nonDeveloper);
+        //Assert
+        List<User> codeReviewers = pullRequest.getCodeReviewers();
+        assertTrue(codeReviewers.contains(nonDeveloper));
+    }
+}
