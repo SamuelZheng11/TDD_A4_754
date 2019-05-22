af385014b8dae61478d4559df6e30400a4c5559a
Sam <samuelzheng11@gmail.com>
2019-05-22 12:49:18 +1200

    make requirement 5 test red

--- src/main/java/GitBranch.java
+++ src/main/java/GitBranch.java
@@ -1,7 +1,13 @@
 public class GitBranch  implements IBranch {
 
+    private String _name;
+
     public GitBranch(String name, GitCommit[] commits){
+        _name = name;
     }
 
+    public String getName() {
+        return _name;
+    }
 
 }
