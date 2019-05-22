4024041edff9efafbc7e2d641bc57c396b98162c
Sam <samuelzheng11@gmail.com>
2019-05-22 14:00:46 +1200

    make test for requirement 7 red

---
+++ src/main/java/MockAbstractionExtension.java
@@ -0,0 +1,10 @@
+public class MockAbstractionExtension implements IAbstractionExtension {
+
+    public void setBranch(IBranch branch) {
+
+    }
+
+    public String generateCodeAbstraction() {
+        return null;
+    }
+}
