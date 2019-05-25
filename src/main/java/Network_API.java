dc270ea29fe955c55c5d8a39417648ac27071ad8
svathi09 <svathis09@gmail.com>
2019-05-25 18:09:43 +1200

    make tests compile red

--- src/main/java/Network_API.java
+++ src/main/java/Network_API.java
@@ -2,6 +2,11 @@
 public interface Network_API {
 	//requirement 11
 		String CreateConnection();
+	//requirement 12
 		String Auto_ReviewSent(User nonDeveloper, String str1);
 		String Auto_ReviewRecieved();
+	//requirement 13
+		String NonDev_ReviewSent(User developer, String str2);
+		String NonDev_ReviewRecieved();
+
 }
