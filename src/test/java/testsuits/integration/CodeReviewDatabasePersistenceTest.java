0299dd2af73d1c440b99de6d4b6fbc64aa0b999e
JennyLee <0608jennylee@hotmail.co.nz>
2019-05-30 15:56:53 +1200

    [RED] add integration test for requirement 8

--- src/test/java/testsuits/integration/CodeReviewDatabasePersistenceTest.java
+++ src/test/java/testsuits/integration/CodeReviewDatabasePersistenceTest.java
@@ -1,30 +1,54 @@
 package testsuits.integration;
 
-import developer.ReviewerPersistence;
 import org.junit.Before;
 import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import developer.CodeReviewAllocation;
+import developer.ReviewerPersistence;
+import github.*;
+import mocks.MockGithubModule;
 
 import com.mongodb.MongoClient;
 
 import java.util.List;
 
 public class CodeReviewDatabasePersistenceTest {
+
+    private String sourceBranchName = "GithubPullRequestFetchTest_Branch";
+    private String targetBranchName = "GithubPullRequestFetchTest_TargetBranch";
+
+    private User developer = new User("", UserType.Developer);
+    private User nonDeveloper = new User("codeReviewer", UserType.NonDeveloper);
+
+    private GitCommit commit = new GitCommit("Commit Message", "GithubPullRequestFetchTest_Commit");
+    private GitCommit[] committed_code = {commit};
+    private GitBranch sourceBranch = new GitBranch(sourceBranchName, committed_code);
+    private GitBranch targetBranch = new GitBranch(targetBranchName, committed_code);
+
     private ReviewerPersistence rp;
 
-    MongoClient mongoClient;
-    String usersDBName;
-    String usersCollectionName;
+    private MongoClient mongoClient;
+    private String usersDBName;
+    private String usersCollectionName;
+    private GithubApi _github;
 
     @Before
     public void setUp(){
+
         //Given
+
         mongoClient = new MongoClient("localhost", 27017);
         usersDBName = "users-db";
         usersCollectionName = "users-collection";
+
         rp = ReviewerPersistence.getInstance();
         rp.addDatabase(mongoClient, usersDBName, usersCollectionName);
+
+        _github = new MockGithubModule();
+
     }
 
     @Test
@@ -41,4 +65,22 @@ public class CodeReviewDatabasePersistenceTest {
         assertEquals(usersDBName, dbName);
     }
 
+    @Test
+    public void shouldUpdateDatabaseWhen(){
+        //Given
+
+        PullRequest mockPullRequest = _github.createPullRequest("Test developer can add code reviewers", sourceBranch, targetBranch);
+        int initialReviewCount = nonDeveloper.getReviewCount();
+
+        //When
+        CodeReviewAllocation codeReviewAllocation = mockPullRequest.createCodeReview(developer, nonDeveloper);
+
+        //Then
+        List<User> codeReviewers = mockPullRequest.getCodeReviewers();
+        assertTrue(codeReviewers.contains(nonDeveloper));
+
+        assertEquals(initialReviewCount+1,nonDeveloper.getReviewCount());
+
+        System.out.println("User Code Review Count save action perfomed!!!");
+    }
 }
